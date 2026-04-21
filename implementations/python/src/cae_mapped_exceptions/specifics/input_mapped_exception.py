21867a8c2bfe80741500d3c98023be539a749339
Ze Lucio Jr <joselucioalmeidajunior@gmail.com>
2026-04-21 18:54:19 -0300

    feat: first steps

---
+++ implementations/python/src/cae_mapped_exceptions/specifics/input_mapped_exception.py
@@ -0,0 +1,7 @@
+
+from ..mapped_exception import MappedException
+
+class InputMappedException(MappedException):
+
+    def __init__(self, brief_public_message: str | None = None, details: str | None = None, original_exception: Exception  | None = None):
+        super().__init__(brief_public_message, details, original_exception)
---
+++ implementations/python/src/cae_mapped_exceptions/specifics/not_authorized_mapped_exception.py
@@ -0,0 +1,6 @@
+from ..mapped_exception import MappedException
+
+class NotAuthorizedMappedException(MappedException):
+
+    def __init__(self, brief_public_message: str | None = None, details: str | None = None, original_exception: Exception  | None = None):
+        super().__init__(brief_public_message, details, original_exception)
